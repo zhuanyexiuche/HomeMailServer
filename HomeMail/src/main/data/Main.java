7b2925daf2f72686a194eeeec59ede9c439ad3d5
4thcalabash <751188973@qq.com>
2018-03-01 19:15:58 +0800

    允许emoji字符

--- HomeMail/src/main/data/Main.java
+++ HomeMail/src/main/data/Main.java
@@ -7,6 +7,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+        Response r = new Response();
+        r.setWXID("test");
+        r.setWXNickName("\uD83D\uDCAF");
+        r.setAvatarUrl("https://wx.qlogo.cn/mmopen/vi_32/ed8NV5iaJqHkDay91hxeWaMVE1Dm8kh0C63gwaRaNcMcPdTUMnicULAj3AFV8LgF766phwSaQz9mFxb5vibYqTLHw/0");
+        r.setBriefContent("test");
+        r.setDeleted(false);
+        r.setQID(8);
+        r.setNiMing(false);
+        r.setCommentCount(0);
+        r.setClapCount(0);
+        r.setContent("test");
+        ResponseDBHelper.getInstance().add(r);
+        /*
         Question q = new Question();
         q.setClapCount(0);
         q.setContext("如题");
@@ -14,6 +27,7 @@ public class Main {
         q.setRespCount(0);
         q.setTopic("为什么大家都上学了，我还在家里玩耍");
         QuestionDBHelper.getInstance().add(q);
+        */
         /*
         ArrayList<Question>list = QuestionDBHelper.getInstance().getAllBrief();
         Response r = new Response();
