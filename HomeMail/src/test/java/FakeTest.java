a8207f571078cb6efea70ef6b462230ba7f72d78
4thcalabash <751188973@qq.com>
2018-03-15 21:58:10 +0800

    a

--- HomeMail/src/test/java/FakeTest.java
+++ HomeMail/src/test/java/FakeTest.java
@@ -1,11 +1,12 @@
 package test.java;
 
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FakeTest {
     @Test
     public void test(){
-        assert(1==1);
+        assertTrue(1==2);
     }
 }
