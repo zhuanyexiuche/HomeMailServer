78d7e46b1138fe54514c18083cb0a5bbc8187102
4thcalabash <751188973@qq.com>
2018-03-15 22:53:23 +0800

    a

--- HomeMail/src/test/java/FakeTest.java
+++ HomeMail/src/test/java/FakeTest.java
@@ -2,11 +2,17 @@ package test.java;
 
 
 import static org.junit.Assert.*;
+
+import main.common.Clap;
 import org.junit.Test;
 
 public class FakeTest {
     @Test
     public void test(){
         assertTrue(2==2);
+        Clap c = new Clap();
+        c.setWXID("123");
+        c.setRID(456);
+        assertTrue(c.getWXID().equals("123"));
     }
 }
