4552d5ce1b66f64110dd26dcdb2d522c636ddef1
4thcalabash <751188973@qq.com>
2018-03-15 21:15:49 +0800

    a

--- HomeMail/src/test/FakerTest.java
+++ HomeMail/src/test/FakerTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 public class FakerTest {
     @Test
     public void test(){
-        assert(1==0);
+        assert 1==2;
+
+    }
+    @Test
+    public void test2(){
+        assert 1==1;
     }
 }
