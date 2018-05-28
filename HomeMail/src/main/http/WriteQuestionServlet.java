7d485a232b571109423ba96f3d84eb2a1b3fc5d4
161250136 <161250136@smail.nju.edu.cn>
2018-05-28 17:57:14 +0800

    Question add two columns

--- HomeMail/src/main/http/WriteQuestionServlet.java
+++ HomeMail/src/main/http/WriteQuestionServlet.java
@@ -13,7 +13,11 @@ public class WriteQuestionServlet extends BaseServlet {
     void work(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String topic = req.getParameter("topic");
         String context = req.getParameter("context").replace("\n","\\n");
+        String owner = req.getParameter("owner");
+        String avater = req.getParameter("ownerAvater");
         Question q = new Question();
+        q.setOwner(owner);
+        q.setOwnerAvater(avater);
         q.setClapCount(0);
         q.setContext(context);
         q.setRespCount(0);
